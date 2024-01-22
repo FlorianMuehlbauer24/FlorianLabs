f4b09fbefba2cbbc1fdf3d8ec4d1024069b8975b
FlorianMuehlbauer24 <155968544+FlorianMuehlbauer24@users.noreply.github.com>
2024-01-22 22:46:53 +0100

    r

--- Week 3/seeding.sql
+++ Week 3/seeding.sql
@@ -1,2 +1,9 @@
-use Car_sales
+use Car_sales;
 
+INSERT INTO Cars (VIN, Manufacturer, Model, Year_Car, Color)
+VALUES ('K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+		('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+		('RKXVNNIHLVVZOUB4M',	'Ford',	'Fusion', 2018, 'White'),
+		('HKNDGS7CU31E9Z7JW',	'Toyota', 'RAV4', 2018, 'Silver'),
+		('DAM41UDN3CHU2WVF6', 'Volvo'	'V60', 2019, 'Gray'),
+		('DAM41UDN3CHU2WVF6',	'Volvo'	'V60' 'Cross Country', 2019,'Gray');
