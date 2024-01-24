27a6de686701748503434726618d3d9677ce9ed1
FlorianMuehlbauer24 <155968544+FlorianMuehlbauer24@users.noreply.github.com>
2024-01-24 12:53:25 +0100

    f

--- Week 3/seeding.sql
+++ Week 3/seeding.sql
@@ -7,3 +7,11 @@ VALUES ('K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
 		('HKNDGS7CU31E9Z7JW',	'Toyota', 'RAV4', 2018, 'Silver'),
 		('DAM41UDN3CHU2WVF6', 'Volvo'	'V60', 2019, 'Gray'),
 		('DAM41UDN3CHU2WVF6',	'Volvo'	'V60' 'Cross Country', 2019,'Gray');
+
+
+INSERT INTO Customers (Customer_ID, Customer_Name, Phone_Number, Email, City, County, ZIP_Code)
+VALUES ('10001', 'Pablo Picasso', '+34 636 17 63 82', EMAIL?
+
+	Madrid	Madrid	Spain	28045
+1	20001	Abraham Lincoln	+1 305 907 7086	-	120 SW 8th St	Miami	Florida	United States	33130
+2	30001	Napoléon Bonaparte	+33 1 79 75 40 00	-	40 Rue du Colisée	Paris	Île-de-France	France	75008
